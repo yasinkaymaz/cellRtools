46891f7a59252041b75e34a100c721b622b82cab
Yasin Kaymaz <yasinkaymaz@github.com>
2018-12-02 19:40:13 -0500

    added function 'var_as_string' to extract variable name as string

--- main/functions.R
+++ main/functions.R
@@ -626,6 +626,8 @@ CellTyper2 <- function(SeuratObject, testExpSet, model, priorLabels, outputFilen
   }#Closes missing(SeuratObj)
 }#closes the function
 
+var_as_string <- function(x) {deparse(substitute(x))}
+
 HTyper <- function(SeuratObject, testExpSet, models, priorLabels, outputFilename="plotpredictions"){
   
   #models is a list of of rf models
@@ -642,10 +644,9 @@ HTyper <- function(SeuratObject, testExpSet, models, priorLabels, outputFilename
   
   colnames(testExpSet) <- make.names(colnames(testExpSet))
   
-  
   Htable <- data.frame(cells = rownames(testExpSet))
   for (model in models){
-    modelname <- deparse(substitute(model))
+    modelname <- var_as_string(model)
     
     print(paste("Predicting with model",modelname,"...",sep = " "))
     
@@ -713,7 +714,7 @@ HTyper <- function(SeuratObject, testExpSet, models, priorLabels, outputFilename
       
       p5 <- ggplot(crx,aes_string(y = "n", axis1 = names(crx)[1], axis2 = names(crx)[2], axis3 = names(crx)[3], axis4 = names(crx)[4] )) +
         geom_alluvium(aes_string(fill = names(crx)[4]), width = 1/12) +
-        geom_stratum(width = 1/12, fill = "black", color = "grey") +
+        geom_stratum(width = 1/12, fill = "black", color = "red") +
         geom_label(stat = "stratum", label.strata = TRUE) +
         scale_x_discrete(limits = names(crx), expand = c(.05, .05)) +
         ggtitle("Predictions Cross-Check")
