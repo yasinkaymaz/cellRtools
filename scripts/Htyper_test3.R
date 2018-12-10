aa83673c955449d10523cdaee512146228f92092
Yasin Kaymaz <yasinkaymaz@github.com>
2018-12-11 00:47:56 +0100

    update

--- scripts/Htyper_test3.R
+++ scripts/Htyper_test3.R
@@ -16,21 +16,21 @@ TaxonomyRankTable <- read.delim("~/codes/test/RF/TaxonomyRank.tree.zeisel.txt", 
 
 models.list <- list(zeisel.rank1.rfcv,zeisel.rank2.rfcv,zeisel.rank3.rfcv,zeisel.rank4.rfcv)
 
-ho <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Ho2018/Ho2018.seurat.Robj"))
-ho <- HTyper22(SeuratObject = ho, taxTable=TaxonomyRankTable, models = models.list, priorLabels = ho@meta.data$res.1, outputFilename = paste("ho",".rfcv.predictions.w-HM",sep = ""))
-rm(ho)
+# ho <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Ho2018/Ho2018.seurat.Robj"))
+# ho <- HTyper22(SeuratObject = ho, taxTable=TaxonomyRankTable, models = models.list, priorLabels = ho@meta.data$res.1, outputFilename = paste("ho",".rfcv.predictions.w-HM",sep = ""))
+# rm(ho)
+#
+# tasic2018 <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Tasic2018/Tasic2018.seurat.Robj"))
+# tasic2018 <- HTyper22(SeuratObject = tasic2018, taxTable=TaxonomyRankTable, models = models.list, priorLabels = tasic2018@meta.data$res.1, outputFilename = paste("tasic2018",".rfcv.predictions.w-HM",sep = ""))
+# rm(tasic2018)
+#
+# Scer <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Saunders/P60Cerebellum_ALT.seurat.Robj"))
+# Scer <- HTyper22(SeuratObject = Scer, taxTable=TaxonomyRankTable, models = models.list, priorLabels = Scer@meta.data$res.1, outputFilename = paste("Saunders-P60Cerebellum_ALT",".rfcv.predictions.w-HM",sep = ""))
+# rm(Scer)
 
-tasic2018 <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Tasic2018/Tasic2018.seurat.Robj"))
-tasic2018 <- HTyper22(SeuratObject = tasic2018, taxTable=TaxonomyRankTable, models = models.list, priorLabels = tasic2018@meta.data$res.1, outputFilename = paste("tasic2018",".rfcv.predictions.w-HM",sep = ""))
-rm(tasic2018)
-
-Scer <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Saunders/P60Cerebellum_ALT.seurat.Robj"))
-Scer <- HTyper22(SeuratObject = Scer, taxTable=TaxonomyRankTable, models = models.list, priorLabels = Scer@meta.data$res.1, outputFilename = paste("Saunders-P60Cerebellum_ALT",".rfcv.predictions.w-HM",sep = ""))
-rm(Scer)
-
-Scor <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Saunders/P60Cortex_noRep5_FRONTALonly.seurat.Robj"))
-Scor <- HTyper22(SeuratObject = Scor, taxTable=TaxonomyRankTable, models = models.list, priorLabels = Scor@meta.data$res.1, outputFilename = paste("Saunders-P60Cortex_noRep5_FRONTALonly",".rfcv.predictions.w-HM",sep = ""))
-rm(Scor)
+#Scor <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Saunders/P60Cortex_noRep5_FRONTALonly.seurat.Robj"))
+#Scor <- HTyper22(SeuratObject = Scor, taxTable=TaxonomyRankTable, models = models.list, priorLabels = Scor@meta.data$res.1, outputFilename = paste("Saunders-P60Cortex_noRep5_FRONTALonly",".rfcv.predictions.w-HM",sep = ""))
+#rm(Scor) too large file
 
 Sstr <- get(load("/n/home13/yasinkaymaz/LabSpace/testdata/GEO/Saunders/P60Striatum.seurat.Robj"))
 Sstr <- HTyper22(SeuratObject = Sstr, taxTable=TaxonomyRankTable, models = models.list, priorLabels = Sstr@meta.data$res.1, outputFilename = paste("Saunders-P60Striatum",".rfcv.predictions.w-HM",sep = ""))
